1277651f058003baac8423439438db11fcbc95be
Martin Palko <palk0001@algonquinlive.com>
2015-01-24 22:20:35 +0000

    - Tweaked builder prefab. - Setup architect with segments.
    git-svn-id: https://subversion.assembla.com/svn/global-game-jam-2015/trunk@64 4145d9ed-1771-4ebd-a01b-bb4d6ffc1650

--- Assets/Game/Scripts/ArchitectPawn.cs
+++ Assets/Game/Scripts/ArchitectPawn.cs
@@ -4,8 +4,9 @@ using System.Collections.Generic;
 
 public class ArchitectPawn : MonoBehaviour
 {
-    int totemHieght = 3;
+    int totemHeight = 3;
 
+    public GameObject totemSegmentPrefab;
     List<Transform> totemSegments = new List<Transform>();
 
     private int health;
@@ -14,15 +15,46 @@ public class ArchitectPawn : MonoBehaviour
     private bool isDead = false;
     public bool IsDead { get { return isDead; } }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start()
+    {
+        UpdateTotemSegmentVisuals();
+    }
+
+    void UpdateTotemSegmentVisuals()
+    {
+        if (totemSegments.Count < totemHeight)
+        {
+            for (int i = totemSegments.Count; i < totemHeight; i++)
+            {
+                GameObject newSegment = Instantiate(totemSegmentPrefab) as GameObject;
+
+                newSegment.transform.SetParent(this.transform);
+                newSegment.transform.localPosition = new Vector3(0, i, 0);
+
+                totemSegments.Add(newSegment.transform);
+            }
+        }
+        else if (totemSegments.Count > totemHeight)
+        {
+            for (int i = totemHeight; i < totemSegments.Count; i++)
+            {
+                Destroy(totemSegments[i].gameObject);
+                totemSegments.RemoveAt(i);
+            }
+        }
+    }
+
+    void AddTotemSegment()
+    {
+        totemHeight++;
+        UpdateTotemSegmentVisuals();
+    }
+
+    void RemoveTotemSegment()
+    {
+        totemHeight--;
+        UpdateTotemSegmentVisuals();
+    }
 
     public void TakeDamage(int aDamage)
     {
