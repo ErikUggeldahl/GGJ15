69a982a8d5a2a094d932d54dd5d51ea2916fc5ba
Martin Palko <palk0001@algonquinlive.com>
2015-01-24 20:25:47 +0000

    - Updated prefab for tower. WIP tower script.
    git-svn-id: https://subversion.assembla.com/svn/global-game-jam-2015/trunk@51 4145d9ed-1771-4ebd-a01b-bb4d6ffc1650

--- Assets/Tower.cs
+++ Assets/Tower.cs
@@ -3,5 +3,32 @@ using System.Collections;
 
 public class Tower : Building
 {
+	public float TowerRange;
 
+	public Transform TurretObject;
+
+    public Transform Target;
+
+
+	GameObject FindTarget()
+	{
+        return null;
+	}
+
+	void Update()
+	{
+		if (CurrentBuildingState == BuildingState.Finished)
+			OrientTurret ();
+	}
+
+	void OrientTurret()
+	{
+		if (TurretObject == null)
+			return;
+
+		Vector3 lookDirection = transform.position - TurretObject.position;
+		lookDirection.y = 0;
+
+		TurretObject.transform.rotation = Quaternion.LookRotation (lookDirection);
+	}
 }
