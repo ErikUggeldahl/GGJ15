46bd7c283fbb70768b1e2bbfe45f1ecfb4b704ef
Erik Uggeldahl <erikuggeldahl@gmail.com>
2015-01-24 18:50:48 +0000

    Added tower building and the architect menu.
    git-svn-id: https://subversion.assembla.com/svn/global-game-jam-2015/trunk@41 4145d9ed-1771-4ebd-a01b-bb4d6ffc1650

--- Assets/Game/Scripts/Architect/BuildingPlacer.cs
+++ Assets/Game/Scripts/Architect/BuildingPlacer.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class BuildingPlacer : MonoBehaviour
 {
+    public enum BuildingType
+    {
+        Wall,
+        Tower
+    }
+
     [SerializeField]
     GameObject wallPreviewObj;
 
@@ -10,21 +16,64 @@ public class BuildingPlacer : MonoBehaviour
     GameObject wallObj;
 
     [SerializeField]
+    GameObject towerPreviewObj;
+
+    [SerializeField]
+    GameObject towerObj;
+
+    [SerializeField]
     Camera camera;
 
     private Transform wallPreview;
+    private Transform towerPreview;
+    private Transform currentPreview;
+    private GameObject currentToBuild;
 
     private int groundLayer;
+
+    private BuildingType currentBuildingType = BuildingType.Wall;
+    private bool isBuilding = false;
 
     void Start()
     {
         groundLayer = LayerMask.NameToLayer("Ground");
 
         wallPreview = ((GameObject)Instantiate(wallPreviewObj)).transform;
+        wallPreview.gameObject.SetActive(false);
+
+        towerPreview = ((GameObject)Instantiate(towerPreviewObj)).transform;
+        towerPreview.gameObject.SetActive(false);
+    }
+
+    public void StartBuildingWall()
+    {
+        StartBuilding(BuildingType.Wall);
+    }
+
+    public void StartBuildingTower()
+    {
+        StartBuilding(BuildingType.Tower);
+    }
+
+    public void StartBuilding(BuildingType buildingType)
+    {
+        currentBuildingType = buildingType;
+        isBuilding = true;
+
+        UpdatePreview(buildingType);
+    }
+
+    private void StopBuilding()
+    {
+        isBuilding = false;
+        currentPreview.gameObject.SetActive(false);
     }
 
     void Update()
     {
+        if (!isBuilding)
+            return;
+
         RaycastHit hitInfo;
         var mouseRay = camera.ScreenPointToRay(Input.mousePosition);
 
@@ -36,7 +85,7 @@ public class BuildingPlacer : MonoBehaviour
         var gridPos = GameGrid.Instance.WorldToGridPosition(hitInfo.point);
         var gridWorldPos = GameGrid.Instance.GridToWorldSpace(gridPos);
 
-        wallPreview.position = gridWorldPos;
+        currentPreview.position = gridWorldPos;
 
         if (Input.GetMouseButtonDown(0))
         {
@@ -44,9 +93,28 @@ public class BuildingPlacer : MonoBehaviour
 
             if (gridSquare.ResidingObject == null)
             {
-                var wallGO = (GameObject)Instantiate(wallObj, gridWorldPos, Quaternion.identity);
+                var wallGO = (GameObject)Instantiate(currentToBuild, gridWorldPos, Quaternion.identity);
                 gridSquare.ResidingObject = wallGO;
+
+                StopBuilding();
             }
         }
+    }
+
+    void UpdatePreview(BuildingType buildingType)
+    {
+        switch (buildingType)
+        {
+            case BuildingType.Wall:
+                currentPreview = wallPreview;
+                currentToBuild = wallObj;
+                break;
+            case BuildingType.Tower:
+                currentPreview = towerPreview;
+                currentToBuild = towerObj;
+                break;
+        }
+
+        currentPreview.gameObject.SetActive(true);
     }
 }
