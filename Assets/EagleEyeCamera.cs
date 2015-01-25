a11b977332ecc6bfd26a8e8977bab340234cd125
Cameron Gazey <camerongazey@live.ca>
2015-01-25 19:18:26 +0000

    - Camera
    git-svn-id: https://subversion.assembla.com/svn/global-game-jam-2015/trunk@133 4145d9ed-1771-4ebd-a01b-bb4d6ffc1650

--- Assets/EagleEyeCamera.cs
+++ Assets/EagleEyeCamera.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class EagleEyeCamera : MonoBehaviour
 {
     private float highestDistance = 0.0f;
+	private float cameraFOV = 0.0f;
+	private float cameraFOVPercentage = 0.0f;
 
     void Update()
     {
@@ -14,19 +16,18 @@ public class EagleEyeCamera : MonoBehaviour
             if (Vector3.Distance(Game.Instance.BuilderPawns[i].transform.position, Game.Instance.ArchitectPawn.transform.position) > highestDistance)
             {
                 highestDistance = Vector3.Distance(Game.Instance.BuilderPawns[i].transform.position, Game.Instance.ArchitectPawn.transform.position);
-                highestDistance = Mathf.Clamp(highestDistance, 10.0f, 100.0f);
-                highestDistance = highestDistance / 100.0f;
-
-                highestDistance = -((-highestDistance - 10.0f) / (25.0f - 10.0f));
-                Debug.Log(highestDistance);
             }
         }
+
+		cameraFOVPercentage = Mathf.Clamp(highestDistance, 8.0f, 30.0f);
+		cameraFOVPercentage = (cameraFOVPercentage - 8.0f) / (30.0f - 8.0f);
+		cameraFOV = (cameraFOVPercentage * 10) + 5.0f;
     }
 
     void LateUpdate()
     {
         this.transform.position = new Vector3(Game.Instance.ArchitectPawn.transform.position.x, this.transform.position.y, this.transform.position.z);
 
-        Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, highestDistance * 0.7f, 5.0f * Time.deltaTime);
+		Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, cameraFOV, 5.0f * Time.deltaTime);
     }
 }
