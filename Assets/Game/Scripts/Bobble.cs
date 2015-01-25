d5bd5ad26bef43652bd9ec647771de148731277c
Martin Palko <palk0001@algonquinlive.com>
2015-01-25 19:15:25 +0000

    - Tweaked bobble.
    git-svn-id: https://subversion.assembla.com/svn/global-game-jam-2015/trunk@132 4145d9ed-1771-4ebd-a01b-bb4d6ffc1650

--- Assets/Game/Scripts/Bobble.cs
+++ Assets/Game/Scripts/Bobble.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class Bobble : MonoBehaviour 
 {
+    public AIMovement aiMovement;
     public Transform MeshTransform;
 
     public float frequency = 5.0f;
     public float angle = 5.0f;
+
+    float localTime;
 
     Vector2 currentPosition;
     Vector2 currentVelocity;
@@ -21,10 +24,16 @@ public class Bobble : MonoBehaviour
 
     void Update()
     {
+        // Hacky as all hell.
+        if (aiMovement && aiMovement.frozenMesh)
+            IsBobbling = !aiMovement.frozenMesh.activeInHierarchy;
+
+        localTime += Time.deltaTime;
+
 		if (!IsBobbling)
 			return;
 
-        Vector2 targetVector = new Vector3(Mathf.Sin(Time.time * frequency) * angle, 0);
+        Vector2 targetVector = new Vector3(Mathf.Sin(localTime * frequency) * angle, 0);
 
 
         // Spring
@@ -36,15 +45,4 @@ public class Bobble : MonoBehaviour
         currentPosition += currentVelocity * Time.deltaTime;
         MeshTransform.localEulerAngles = new Vector3(currentPosition.x, 0, currentPosition.y);
     }
-
-	public void StartBobblingAfterTime(float time)
-	{
-		StartCoroutine(BobbleAfterTime(time));
-	}
-
-	private IEnumerator BobbleAfterTime(float time)
-	{
-		yield return new WaitForSeconds(time);
-		IsBobbling = true;
-	}
 }
